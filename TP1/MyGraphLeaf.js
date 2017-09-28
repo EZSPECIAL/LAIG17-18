82634296f390055f7bda05a12121f2fb4018ee18
EZSPECIAL <EZSPECIAL@users.noreply.github.com>
2017-09-28 17:23:15 +0100

    Initial files

---
+++ TP1/MyGraphLeaf.js
@@ -0,0 +1,35 @@
+/**
+ * MyGraphLeaf class, representing a leaf in the scene graph.
+ * @constructor
+**/
+
+function MyGraphLeaf(graph, xmlelem) {
+	
+	
+	var type=graph.reader.getItem(xmlelem, 'type', ['rectangle', 'cylinder', 'sphere', 'triangle']);
+
+	
+
+	switch(type) {
+		case 'triangle':
+			var coords = [];
+			var errorCheck;
+			var argString = graph.reader.getString(xmlelem, 'args', errorCheck);
+			var splitted = argString.split(' ');
+			for(var i = 0; i < splitted.length; i+=3) {
+				coords.push(vec3.fromValues(
+				parseFloat(splitted[i]),
+				parseFloat(splitted[i+1]),
+				parseFloat(splitted[i+2])));
+			}
+			console.log("VEC3:" + coords[0]);
+						console.log("VEC3:" + coords[1]);
+									console.log("VEC3:" + coords[2]);
+
+			// for(var i = 0; i < lines.length; i++) 
+			// for(int i 
+			// console.log("COORDS:" + coords);
+		break;
+	}
+	
+}
