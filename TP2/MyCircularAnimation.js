9d3989f3652327c616cc20e762ff2b271d85cc2c
Luís Martins <luis.n.martins97@gmail.com>
2017-11-16 18:34:27 +0000

    Circular Animation

--- TP2/MyCircularAnimation.js
+++ TP2/MyCircularAnimation.js
@@ -9,9 +9,51 @@ function MyCircularAnimation(id, speed, center, radius, initAngle, rotationAngle
 	
 	this.center = center;
 	this.radius = radius;
-	this.initAngle = initAngle;
-	this.rotationAngle = rotationAngle;
+	this.initAngle = initAngle * Math.PI / 180;
+	this.rotationAngle = rotationAngle * Math.PI / 180;
+	console.log(this.initAngle);
+	console.log(this.rotationAngle);
+
 };
 
 MyCircularAnimation.prototype = Object.create(MyAnimation.prototype);
 MyCircularAnimation.prototype.constructor = MyCircularAnimation;
+
+
+/**
+ * 
+ * Get Animation Matrix time seconds after animation's start
+ *
+ * @param time Time in seconds, after animation's start
+ */
+MyCircularAnimation.prototype.getAnimationMatrix = function(time){
+
+	let totalAngle;
+	let matrix = mat4.create();
+	mat4.identity(matrix);
+
+	let w = this.speed / this.radius;
+
+	let incAngle = (w * time) * Math.PI / 180;
+
+	if(incAngle < this.rotationAngle){
+	
+		totalAngle = this.initAngle + incAngle;
+
+	} else{
+
+		totalAngle = this.initAngle + this.rotationAngle;
+	}
+
+	let translateOriginVector = vec3.fromValues(-this.center[0], 0, -this.center[2]);
+	mat4.translate(matrix, matrix, translateOriginVector);
+
+	let rotateAxis = vec3.fromValues(0, 1, 0);
+	mat4.rotate(matrix, matrix, totalAngle, rotateAxis);
+
+	let reverseTranslate = vec3.fromValues(this.center[0], 0, this.center[2]);
+	mat4.translate(matrix, matrix, reverseTranslate);
+
+	return matrix;
+
+}
