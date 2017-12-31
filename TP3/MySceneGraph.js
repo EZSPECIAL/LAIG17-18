6f82dae416ae9058df9d327829654ea43c314833
EZSPECIAL <ftlopes93@gmail.com>
2017-12-31 16:07:49 +0000

    Hide picking cells

--- TP3/MySceneGraph.js
+++ TP3/MySceneGraph.js
@@ -1800,7 +1800,7 @@ MySceneGraph.generateRandomString = function(length) {
  */
 MySceneGraph.prototype.registerPicking = function() {
     
-    //this.textures["cellAlpha"][0].bind();
+    this.textures["cellAlpha"][0].bind();
     
     for(let y = 0; y < 12; y++) {
         for(let x = 0; x < 12; x++) {
@@ -1825,7 +1825,7 @@ MySceneGraph.prototype.registerPicking = function() {
         }
     }
     
-    //this.textures["cellAlpha"][0].unbind();
+    this.textures["cellAlpha"][0].unbind();
     this.scene.clearPickRegistration();
 }
 
